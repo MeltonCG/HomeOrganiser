9290349d21e19076917b54a78cee1ffa930b7ee5
Matt Felton <Matthew.felton.se@outlook.com>
2022-10-16 19:49:10 +0100

    Added Fontawesome & No data component

---
+++ HomeOrganiser.Core/Attributes/EnumAttributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeOrganiser.Core.Attributes
+{
+    public class DisplayDescription : Attribute
+    {
+        public string NameToShow { get; set; }
+
+        public DisplayDescription(string _nameToShow)
+        {
+            NameToShow = _nameToShow;
+        }
+    }
+}
---
+++ HomeOrganiser/Enums/IconEnums.cs
@@ -0,0 +1,45 @@
+﻿using HomeOrganiser.Core.Attributes;
+
+namespace HomeOrganiser.Enums
+{
+    public enum EIconWeight
+    {
+        [DisplayDescription("fas")]
+        Solid,
+        [DisplayDescription("fad")]
+        Duotone,
+        [DisplayDescription("far")]
+        Regular,
+        [DisplayDescription("fal")]
+        Light,
+        [DisplayDescription("fat")]
+        Thin
+    }
+
+    public enum EIconSize
+    {
+        [DisplayDescription("")]
+        Default,
+        [DisplayDescription("fa-xl")]
+        Large,
+        [DisplayDescription("fa-2x")]
+        X2,
+        [DisplayDescription("fa-3x")]
+        X3,
+        [DisplayDescription("fa-4x")]
+        X4,
+        [DisplayDescription("fa-5x")]
+        X5,
+        [DisplayDescription("fa-6x")]
+        X6,
+        [DisplayDescription("fa-7x")]
+        X7,
+        [DisplayDescription("fa-8x")]
+        X8,
+        [DisplayDescription("fa-9x")]
+        X9,
+        [DisplayDescription("fa-10x")]
+        X10
+    }
+    
+}
