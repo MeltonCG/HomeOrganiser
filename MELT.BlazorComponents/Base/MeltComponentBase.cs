0fd8460495d67ad362326ee00099448e93f62816
Matt Felton <Matthew.Felton.SE@outlook.com>
2023-03-19 12:53:29 +0000

    Update components ready for utility creation

---
+++ MELT.BlazorComponents/Base/MeltComponentBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MELT.BlazorComponents.Base
+{
+    public abstract class MeltComponentBase: ComponentBase
+    {
+        /// <summary>
+        /// User class names, separated by space.
+        /// </summary>
+        [Parameter]
+        public string Class { get; set; }
+
+        /// <summary>
+        /// Attributes carries all attributes you add to the component that don't match any of its parameters.
+        /// They will be added to the underlying HTML tag.
+        /// </summary>
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> Attributes { get; set; } = new Dictionary<string, object>();
+    }
+}
---
+++ MELT.BlazorComponents/Enums/ButtonEnums.cs
@@ -0,0 +1,35 @@
+﻿using MELT.BlazorComponents.Attributes;
+
+namespace MELT.BlazorComponents.Enums
+{
+    public class ButtonEnums
+    {
+        public enum EButtonVariant
+        {
+            [DisplayDescription("btn-outline-dark")]
+            Default,
+            [DisplayDescription("btn-primary")]
+            Primary,
+            [DisplayDescription("btn-outline-primary")]
+            PrimaryOutline,
+            [DisplayDescription("btn-outline-secondary")]
+            SecondaryOutline,
+            [DisplayDescription("btn-outline-success")]
+            SuccessOutline,
+            [DisplayDescription("btn-outline-danger")]
+            DangerOutline,
+            [DisplayDescription("btn-outline-info")]
+            InfoOutline
+        }
+
+        public enum EButtonSize
+        {
+            [DisplayDescription("")]
+            Default,
+            [DisplayDescription("btn-sm")]
+            SM,
+            [DisplayDescription("btn-lg")]
+            LG
+        }
+    }
+}
